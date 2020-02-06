bbeba7333027505f222fddc25db8705a94b82bee
zaks <xyglc2014@163.com>
2020-02-06 23:36:49 +0800

    添加sqlite的操作方法

--- CSharp Basics/02.数据库操作/Program.cs
+++ CSharp Basics/02.数据库操作/Program.cs
@@ -5,12 +5,14 @@ namespace _02.数据库操作 {
     class Program {
         static void Main(string[] args)
         {
-            SQLServerHelper sqlServer = new SQLServerHelper();
-            //sqlServer.GetData();
-            DataTable dt = sqlServer.GetAccountList();
-            foreach (DataRow item in dt.Rows) {
-                Console.WriteLine($"{item[0]},{item[1]},{item[2]}");
-            }
+            //SQLServerHelper sqlServer = new SQLServerHelper();
+            ////sqlServer.GetData();
+            //DataTable dt = sqlServer.GetAccountList();
+            //foreach (DataRow item in dt.Rows) {
+            //    Console.WriteLine($"{item[0]},{item[1]},{item[2]}");
+            //}
+            SqliteHelper.demo();
+            
             Console.ReadKey();
         }
     }
--- CSharp Basics/02.数据库操作/SqliteHelper.cs
+++ CSharp Basics/02.数据库操作/SqliteHelper.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Microsoft.Data.Sqlite;
 namespace _02.数据库操作 {
-    class SqliteHelper {
+    /// <summary>
+    /// 操作sqlite数据库 方法和操作sqlsever一样  ,需要先创建数据库
+    /// </summary>
+    public class SqliteHelper {
+        public static readonly string ConnectionString = "Data source=zaks.db";
+        public static void demo() {
+            using (SqliteConnection connection = new SqliteConnection(ConnectionString)) {
+                connection.Open();
+                SqliteCommand cmd = new SqliteCommand("select * from account",connection);
+                SqliteDataReader reader =  cmd.ExecuteReader();
+                if (reader.HasRows) {
+                    while (reader.Read()) {
+                        Console.WriteLine($"{reader[0]},{reader[1]},{reader[2]}");
+                    }
+                }
+            }
+        }
     }
 }
